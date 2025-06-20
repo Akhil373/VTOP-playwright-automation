8aeee5305a14a9bccec217a8edc680f3ec2231e4
Akhil <133588800+Akhil373@users.noreply.github.com>
2025-06-20 22:47:20 +0530

    logic to scrape all months

--- data_processor.py
+++ data_processor.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import os
+import glob
 
 def clean_file(input_file, output_file) -> None: 
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
@@ -9,7 +10,7 @@ def clean_file(input_file, output_file) -> None:
             if stripped_line:
                 outfile.write(stripped_line + '\n')
 
-def html_to_csv(file_path, output_path) -> None: 
+def html_to_csv(file_path, output_path, month) -> None: 
     html_string: str = ""
     with open(file_path) as f:
         html_string = f.read()
@@ -19,16 +20,31 @@ def html_to_csv(file_path, output_path) -> None:
     table_rows= soup.find_all('tr')
 
     for row in table_rows:
-        all_cells = row.find_all(['th', 'td'])
+        all_cells = row.find_all(['th', 'td']) # type: ignore
         cols = [cell.get_text(strip=True) for cell in all_cells]
         data.append(cols)
 
     header = data[0]
     table_data = data[1:]
     df = pd.DataFrame(table_data, columns=header)
+    df.insert(0, 'Month', month)
     
     print("---Academic Calendar---")
     print(df)
     df.to_csv(output_path, index=False) 
     
-    print("\nData successfully saved to academic_calendar.csv") 
+    print("\nData successfully saved to academic_calendar*.csv") 
+
+def combine_csv(input_dir="temp"):
+    file_pattern = os.path.join(input_dir, 'academic_calendar*.csv')
+    output_file = os.path.join("data", 'academic_calendar.csv')
+
+    all_files = glob.glob(file_pattern)
+
+    if not all_files:
+        print(f"No files found with pattern '{file_pattern}'")
+    else:
+        df_list = [pd.read_csv(file) for file in all_files]
+        combined_df = pd.concat(df_list, ignore_index=True)
+        combined_df.to_csv(output_file, index=False)
+        print(f"Successfully combined {len(all_files)} files into '{output_file}'")
